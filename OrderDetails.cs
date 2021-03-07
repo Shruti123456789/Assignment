511f21b2669ea6b30a18b01fc4fff86bf3c8e9e5
Shruti123456789 <64364014+Shruti123456789@users.noreply.github.com>
2021-03-07 21:16:54 +0530

    Add files via upload

---
+++ OrderDetails.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderLibrary
+{
+    public class OrderDetails
+    {
+        private int orderId;
+        public int OrderId
+        {
+            get { return orderId; }
+            set { orderId = value; }
+        }
+
+        int customerId;
+        public int CustomerId
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        int productQuantity;
+        public int ProductQuantity
+        {
+            get { return productQuantity; }
+            set { productQuantity = value; }
+        }
+        float price;
+        public float Price 
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public OrderDetails()
+        {
+            Console.WriteLine("Inside Default Constructor");
+        }
+        public OrderDetails(int id) 
+        {
+            OrderId = id;
+        }
+        public OrderDetails(int id,int cid):this(id)
+        {
+            CustomerId = cid;
+        }
+        public OrderDetails(int id, int cid,int quant):this(id,cid)
+        {
+            ProductQuantity = quant;
+        }
+        public OrderDetails(int id, int cid, int quant,float price) : this(id, cid,quant)
+        {
+            Price = price;
+        }
+        static OrderDetails()
+        {
+            Console.WriteLine("Inside Static Constructor.I will be executed first");
+        }
+        public float CalculateExpense()
+        {
+            float ans = Price * ProductQuantity;
+            return ans;
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderLibrary;
+namespace Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderDetails o = new OrderDetails();
+            OrderDetails o1 = new OrderDetails(1);
+            Console.WriteLine("Order Id=" + o1.OrderId);
+            OrderDetails o2 = new OrderDetails(2, 2);
+            Console.WriteLine("Order Id=" + o2.OrderId);
+            Console.WriteLine("Consumer Id=" + o2.CustomerId);
+            Console.WriteLine("Product Quantity=" + o2.ProductQuantity);
+            Console.WriteLine("price of commodity=" + o2.Price);
+            Console.WriteLine("Total Expense=" + o2.CalculateExpense());
+            OrderDetails o3 = new OrderDetails(3, 3, 8);
+            Console.WriteLine("Order Id=" + o3.OrderId);
+            Console.WriteLine("Consumer Id=" + o3.CustomerId);
+            Console.WriteLine("Product Quantity=" + o3.ProductQuantity);
+            Console.WriteLine("price of commodity=" + o3.Price);
+            Console.WriteLine("Total Expense=" + o3.CalculateExpense());
+            OrderDetails o4 = new OrderDetails(4,2,10,10000);
+            Console.WriteLine("Order Id="+ o4.OrderId);
+            Console.WriteLine("Consumer Id="+ o4.CustomerId);
+            Console.WriteLine("Product Quantity="+o4.ProductQuantity);
+            Console.WriteLine("price of commodity="+o4.Price);
+            Console.WriteLine("Total Expense="+o4.CalculateExpense());
+            Console.Read();
+
+
+        }
+    }
+}
